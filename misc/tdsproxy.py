0737c43a3f804ac6851928464cd1aa61248b0f78
Hajime Nakagami <nakagami@gmail.com>
2016-05-01 09:53:41 +0900

    add print()

--- misc/tdsproxy.py
+++ misc/tdsproxy.py
@@ -26,6 +26,17 @@ import sys
 import socket
 import binascii
 
+TDS_NAME = {
+    1: "TDS_SQL_BATCH",
+    3: "TDS_RPC",
+    4: "TDS_TABULAR_RESULT",
+    6: "TDS_ATTENTION_SIGNALE",
+    7: "TDS_BULK_LOAD_DATA",
+    14: "TDS_TRANSACTION_MANAGER_REQUEST",
+    16: "TDS_LOGIN",
+    18: "TDS_PRELOGIN",
+}
+
 
 def proxy_wire(server_name, server_port, listen_host, listen_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,15 +48,26 @@ def proxy_wire(server_name, server_port, listen_host, listen_port):
 
     while True:
         client_head = client_sock.recv(8)
+        if len(client_head) == 0:
+            break
+        t = client_head[0]
+        status = client_head[1]
         ln = int.from_bytes(client_head[2:4], byteorder='big')
+        spid = int.from_bytes(client_head[4:6], byteorder='big')
         client_body = client_sock.recv(ln)
         server_sock.send(client_head)
         server_sock.send(client_body)
+        print("<<%s:%d, len=%d spid=%d data=%s" % (TDS_NAME[t], len(client_body), status, spid, binascii.b2a_hex(client_body).decode('ascii')))
+
         server_head = server_sock.recv(8)
+        t = server_head[0]
+        status = server_head[1]
         ln = int.from_bytes(server_head[2:4], byteorder='big')
+        spid = int.from_bytes(server_head[4:6], byteorder='big')
         server_body = server_sock.recv(ln)
         client_sock.send(server_head)
         client_sock.send(server_body)
+        print(">>%s:%d, len=%d spid=%d data=%s" % (TDS_NAME[t], status, len(server_body), spid, binascii.b2a_hex(server_body).decode('ascii')))
 
 
 if __name__ == '__main__':
