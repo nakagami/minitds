d885888207a22b1a77bedfb76c070539568b2847
Hajime Nakagami <nakagami@gmail.com>
2019-04-04 09:13:14 +0900

    update copyright year

--- test_minitds.py
+++ test_minitds.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ##############################################################################
 # The MIT License (MIT)
 #
-# Copyright (c) 2016-2017 Hajime Nakagami
+# Copyright (c) 2016-2019 Hajime Nakagami
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
